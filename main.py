b43f2bf8ce8df95dd0c7ca495f64704cb83fd269
Anat94 <61147387+Anat94@users.noreply.github.com>
2020-11-11 22:43:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ ROLE_PERM = 'Modo D'
 CHANNEL_BIENVENUE = 772547033897762837
 LOG = 774627166456512524
 
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix= PREFIX, intents=intents, help_command=None)
 
 @bot.event
 async def on_ready():
